a07e66fa839e88cb73dbbc4b764d8a7dd664abe1
nguyentienloc653 <locnguyentien653@gmail.com>
2024-12-10 13:09:18 +0700

    Update bài 2.c

--- bài 2.c
+++ bài 2.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
-int tinhTong(int a, int b){
-	printf("Tong hai so %d va %d la %d", a, b, a+b);
-	return a+b;
+void inRaCacPhanTuTrongMang(int arr[], int size){
+	for(int i=0; i<size; i++){
+		printf("Phan tu thu %d la %d\n", i+1, arr[i]);
+	}
 }
 int main(){
-	tinhTong(5,5);
+	int number[]={1,2,3,4,5,6,7,8,9};
+	int size= sizeof(number)/sizeof(int);
+	inRaCacPhanTuTrongMang(number,size);
+	
 	return 0;
 }
